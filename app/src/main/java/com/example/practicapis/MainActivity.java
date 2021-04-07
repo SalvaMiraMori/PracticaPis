78c470d838f76e285abafe18453bf23562506062
Laura Garcia <lgarcife.ub@gmail.com>
2021-04-07 09:22:37 +0200

    change button add

--- app/src/main/java/com/example/practicapis/MainActivity.java
+++ app/src/main/java/com/example/practicapis/MainActivity.java
@@ -22,11 +22,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
     }
-
+    public void addNote(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
