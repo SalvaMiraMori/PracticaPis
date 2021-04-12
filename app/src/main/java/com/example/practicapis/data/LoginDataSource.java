9f22d06d062e408ef2dea883ba76c7fd7496c455
Salva Mira Morillas <salvamiramori@gmail.com>
2021-04-12 13:02:59 +0200

    LogIn base implementat

---
+++ app/src/main/java/com/example/practicapis/data/LoginDataSource.java
@@ -0,0 +1,30 @@
+package com.example.practicapis.data;
+
+import com.example.practicapis.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    // TODO: Implement displayName
+                    new LoggedInUser(
+                            username,
+                            username);
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
